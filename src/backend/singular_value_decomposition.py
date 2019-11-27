67bc8f074bd0544288b32dc36e9dc0aad6d38cdc
AdityaGovardhan <dtgovardhan@gmail.com>
2019-11-27 16:42:26 -0700

    fit, fit_trasform and transform defined for svd

--- src/backend/singular_value_decomposition.py
+++ src/backend/singular_value_decomposition.py
@@ -1,13 +1,14 @@
-from database_connection import DatabaseConnection
 import numpy as np
 
 
 # Is a temperarory class design
 class SingularValueDecomposition:
 
-    def __init__(self):
-        self.dbconnection = DatabaseConnection()
-        pass
+    def __init__(self, num_dims=10):
+        self.U = None
+        self.S = None
+        self.Vt = None
+        self.num_dims = num_dims
 
     def get_latent_semantics(self, data_matrix, n_components):
         u, s, vt = self.get_svd_decomposition(data_matrix)
@@ -27,7 +28,32 @@ class SingularValueDecomposition:
         transformed_data = np.matmul(u, s)
         return transformed_data
 
+    def fit(self, data_matrix):
+        self.U, self.S, self.Vt = np.linalg.svd(data_matrix)
+
+    def fit_transform(self, data_matrix):
+        self.U, self.S, self.Vt = np.linalg.svd(data_matrix)
+        vt = self.Vt[:self.num_dims, :]
+        return data_matrix.dot(vt.T)
+
+    def transform(self, data_matrix):
+        vt = self.Vt[:self.num_dims, :]
+        return data_matrix.dot(vt.T)
 
 if __name__ == "__main__":
-    svd_object = SingularValueDecomposition()
-    svd_object.task6_using_svd(27)
+
+    A = np.array([
+    [1,2,3,4,5,6,7,8,9,10],
+    [11,12,13,14,15,16,17,18,19,20],
+    [21,22,23,24,25,26,27,28,29,30]])
+
+    B = np.array([
+    [1,2,3,4,5,6,7,8,9,10],
+    [11,12,13,14,15,16,17,18,19,20]])
+
+    svd_object = SingularValueDecomposition(2)
+    tf_A = svd_object.fit_transform(A)
+    print(tf_A)
+
+    tf_B = svd_object.transform(B)
+    print(tf_B)
