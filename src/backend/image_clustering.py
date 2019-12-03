cdc3e100ff730d0253439dd00300f391a0bf1465
Arihant <sinhaarihant@gmail.com>
2019-12-03 15:23:03 -0700

    Task 2 UI integrated

--- src/backend/image_clustering.py
+++ src/backend/image_clustering.py
@@ -13,6 +13,47 @@ class Image_Clustering:
         self.db_conn = DatabaseConnection()
         self.no_of_dimensions = 20
 
+    def k_means(self, points, no_of_centres):
+
+        list_of_centre = (points.shape[0] * np.random.rand(no_of_centres,1)).tolist()
+        points = points.tolist()
+        clusters_points = {}
+        old_clusters_centroid = {}
+
+        clusters_centroid = { index:points[int(list_of_centre[index][0])] for index in range(len(list_of_centre))}
+
+        while not clusters_centroid == old_clusters_centroid:
+
+            # allocate points to cluster centroid
+            clusters_points = {}
+            for point in points:
+                min_distance = np.inf
+                point_in_cluster = -1
+
+                for cluster_id in clusters_centroid:
+                    centroid = clusters_centroid[cluster_id]
+                    distance = np.linalg.norm(np.subtract(np.array(point), np.array(centroid)))
+
+                    if distance < min_distance:
+                        point_in_cluster = cluster_id
+                        min_distance = distance
+
+                if point_in_cluster in clusters_points:
+                    clusters_points[point_in_cluster].append(point)
+                else:
+                    clusters_points[point_in_cluster] = [point]
+
+            # Update cluster centroid
+            old_clusters_centroid = clusters_centroid.copy()
+            clusters_centroid = {}
+
+            for cluster_id in clusters_points:
+                list_of_points = np.array(clusters_points[cluster_id])
+                clusters_centroid[cluster_id] = np.mean(list_of_points, axis=0).tolist()
+                # print(cluster_id,clusters_centroid[cluster_id])
+
+        return clusters_centroid  # ,clusters_centroid
+
     def cluster_images(self, no_of_clusters, relative_folder_path):
         tablename = convert_folder_path_to_table_name(relative_folder_path, "histogram_of_gradients")
         image_dict = self.db_conn.get_object_feature_matrix_from_db(tablename)
@@ -29,7 +70,8 @@ class Image_Clustering:
         list_of_centers = clustered_data.tolist()
 
         points_in_cluster = []
-        for image_name, image_vector in zip(image_names, list_of_images):
+        sorted_image_list = sorted(zip(image_names, list_of_images), key=lambda k: k[0])
+        for image_name, image_vector in sorted_image_list:
             min_distance = np.inf
             row = ()
             for center in list_of_centers:
@@ -39,9 +81,10 @@ class Image_Clustering:
                     min_distance = distance
             points_in_cluster.append(row)
 
+
         prediction = sorted(points_in_cluster, key=lambda k: k[1])
 
-        #pprint.pprint(prediction)
+        pprint.pprint(prediction)
 
         return prediction
 
@@ -51,7 +94,7 @@ if __name__ == "__main__":
 
     clustering_obj = Image_Clustering()
     prediction = clustering_obj.cluster_images(no_of_clusters, relative_folder_path)
-    pprint.pprint(prediction)
+
 
 
 
