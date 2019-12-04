d8d5cc900ca5a3ceb3a9259477046cd9e13e663c
Arihant <sinhaarihant@gmail.com>
2019-12-03 22:22:25 -0700

    Parameters

--- src/tasks/task2.py
+++ src/tasks/task2.py
@@ -10,7 +10,7 @@ from image_clustering import Image_Clustering
 
 class Task2(CreateView):
     model = models.Task2Model
-    fields = ('number_of_clusters','dataset',)
+    fields = ('number_of_clusters','labelled_dataset','Unlabelled_dataset',)
     template_name = 'task2.html'
 
     def get_context_data(self, **kwargs):
@@ -21,10 +21,11 @@ class Task2(CreateView):
 
 def execute_task2(request):
     no_of_clusters = int(request.POST.get("number_of_clusters"))
-    dataset = request.POST.get("dataset")
+    dataset1 = request.POST.get("labelled_dataset")
+    dataset2 = request.POST.get("Unlabelled_dataset")
 
-    labelled_folder_path = "/Labelled/" + dataset
-    unlabelled_folder_path = "/Unlabelled/" + dataset
+    labelled_folder_path = "/Labelled/" + dataset1
+    unlabelled_folder_path = "/Unlabelled/" + dataset2
 
     clustering_obj = Image_Clustering()
     points_in_cluster, prediction = clustering_obj.cluster_images(no_of_clusters, labelled_folder_path, unlabelled_folder_path)
